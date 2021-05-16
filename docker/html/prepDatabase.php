d8285ab1a1d8f8ae4dac875c9b57d95df62a18f6
Bas <bazzard@gmail.com>
2021-05-16 14:44:34 +0200

    prepdatabase

--- docker/html/prepDatabase.php
+++ docker/html/prepDatabase.php
@@ -9,21 +9,21 @@ $conn = new mysqli($host, $user, $pass, $db);
     die("Connection failed: " . $conn->connect_error);
   }
   $sqlCreateDatabase = "CREATE DATABASE activities";
-  $sqlCreateTable = "CREATE TABLE `strava`.`activities` ( `athleteID` INT NOT NULL,
-  `name` VARCHAR NOT NULL, 
-  `distance` DECIMAL NOT NULL, 
-  `moving_time` INT NOT NULL, 
-  'elapsed_time' INT NOT NULL, 
-  'type' VARCHAR NOT NULL, 
-  'id' INT PRIMARY KEY, 
-  'start_date' VARCHAR NOT NULL, 
-  'start_data_local' VARCHAR NOT NULL, 
-  'timezone' VARCHAR NOT NULL,
-  'trainer' BOOLEAN NOT NULL, 
-  'commute' BOOLEAN NOT NULL, 
-  'manual' BOOLEAN NOT NULL, 
-  'gear_id' VARCHAR NOT NULL),
-  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  $sqlCreateTable = "CREATE TABLE `activities` (
+    `athleteID` int(10) UNSIGNED NOT NULL,
+    `name` varchar(200) NOT NULL,
+    `distance` double(7,1) UNSIGNED NOT NULL,
+    `moving_time` int(8) UNSIGNED NOT NULL,
+    `elapsed_time` int(8) UNSIGNED NOT NULL,
+    `type` varchar(10) NOT NULL,
+    `id` int(10) NOT NULL,
+    `start_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    `start_data_local` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `timezone` varchar(50) NOT NULL,
+    `trainer` tinyint(1) NOT NULL,
+    `commute` tinyint(1) NOT NULL,
+    `manual` tinyint(1) NOT NULL,
+    `gear_id` varchar(8) NOT NULL
   )";
 
   
